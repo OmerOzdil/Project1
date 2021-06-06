452be7201026b062df530d2d23f6e2a225e18f73
OmerOzdil <omerozdil54@gmail.com>
2021-06-06 20:55:11 +0100

    Branch2

--- src/Package/HelloWorld4.java
+++ src/Package/HelloWorld4.java
@@ -3,7 +3,7 @@ package Package;
 public class HelloWorld4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("This is form the Branch 2");
 
 	}
 
